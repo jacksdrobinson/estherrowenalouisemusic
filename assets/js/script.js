c9bfdc16ecc53f2151bf0001c40958233f446f1d
Jack Robinson <jack@wearable.technology>
2020-08-22 22:43:09 +0100

    Eliminate jitter when scrolling to page end

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,25 @@ function debounce ( callback ) {
     }
 }
 
-function storeScroll () {
-    document.documentElement.style.setProperty( "--scroll-position", window.scrollY + "px" );
-}
-
-const titleBarSpacingBlock = document.getElementsByClassName("title-bar-spacing-block")[0];
+const siteMainContainer = document.getElementsByClassName("site-main-container")[0];
 const titleBarContainer = document.getElementsByClassName("title-bar-container")[0];
+const contentContainer = document.getElementsByClassName("content-container")[0];
+const footerContainer = document.getElementsByClassName("footer-container")[0];
 
-function checkTitleBarScroll () {
-    if ( titleBarSpacingBlock.getBoundingClientRect().bottom === titleBarContainer.getBoundingClientRect().bottom ) {
-        titleBarContainer.removeAttribute("data-title-bar-minimized");
-    } else {
+function onScroll () {
+
+    let titleBarMinHeight = parseInt( getComputedStyle( siteMainContainer ).getPropertyValue( "--title-bar-min-height" ));
+    let maxAllowedScroll = Math.floor(( contentContainer.offsetHeight + footerContainer.offsetHeight ) / 2 );
+    let scrollValue = Math.min( maxAllowedScroll, window.scrollY );
+
+    document.documentElement.style.setProperty( "--scroll-position", scrollValue + "px" );
+
+    if ( titleBarMinHeight === titleBarContainer.offsetHeight ) {
         titleBarContainer.dataset.titleBarMinimized = "";
+    } else {
+        titleBarContainer.removeAttribute("data-title-bar-minimized");
     }
 }
 
-function onScroll () {
-    storeScroll();
-    checkTitleBarScroll();
-}
-
-storeScroll();
+onScroll();
 document.addEventListener( "scroll", debounce( onScroll ));
