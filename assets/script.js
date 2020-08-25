66a751a914f03f4baaf130e014db6b0561083fd4
Jack Robinson <jack@wearable.technology>
2020-08-25 22:15:38 +0100

    Compile all pages into single scrolling page

--- assets/script.js
+++ assets/script.js
@@ -9,23 +9,26 @@ function debounce ( callback ) {
     }
 }
 
-const siteMainContainer = document.getElementsByClassName("site-main-container")[0];
-const titleBarContainer = document.getElementsByClassName("title-bar-container")[0];
-const contentContainer = document.getElementsByClassName("content-container")[0];
-const footerContainer = document.getElementsByClassName("footer-container")[0];
+const siteMainContainer = document.getElementsByClassName( "site-main-container" )[0];
+const titleBarContainer = document.getElementsByClassName( "title-bar-container" )[0];
+const contentContainer = document.getElementsByClassName( "content-container" )[0];
+const footerContainer = document.getElementsByClassName( "footer-container" )[0];
+
+console.log( document.documentElement );
 
 function onScroll () {
 
-    let titleBarMinHeight = parseInt( getComputedStyle( siteMainContainer ).getPropertyValue( "--title-bar-min-height" ));
+    // Work out how far the user has scrolled, allowing CSS to use this value
     let maxAllowedScroll = Math.floor(( contentContainer.offsetHeight + footerContainer.offsetHeight ) / 2 );
     let scrollValue = Math.min( maxAllowedScroll, window.scrollY );
-
     document.documentElement.style.setProperty( "--scroll-position", scrollValue + "px" );
 
+    // Work out if the user has scrolled far enough to minimize the title bar, setting an attribute for CSS to use if so
+    let titleBarMinHeight = parseInt( getComputedStyle( siteMainContainer ).getPropertyValue( "--title-bar-min-height" ));
     if ( titleBarMinHeight === titleBarContainer.offsetHeight ) {
         titleBarContainer.dataset.titleBarMinimized = "";
     } else {
-        titleBarContainer.removeAttribute("data-title-bar-minimized");
+        titleBarContainer.removeAttribute( "data-title-bar-minimized" );
     }
 }
 
